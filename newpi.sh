a1102a9255b7adaab35122594411c0083b1b0659
Michael Hill <mhill@betterview.net>
2019-11-28 00:36:52 -0800

    cleanup

--- newpi.sh
+++ newpi.sh
@@ -1,4 +1,4 @@
-#! /usr/bin/env bas
+#! /usr/bin/env bash
 
 USERNAME='pi'
 REBOOT_WAIT_SECS=5
@@ -6,13 +6,13 @@ REBOOT_WAIT_SECS=5
 while getopts ":H:nqU:i:r:" opt; do
   case ${opt} in
     H ) HOSTNAME="${OPTARG}" ;;
-	n ) DRY_RUN=1 ;;
-	q ) QUIET=1 ;;
-	U ) USERNAME="${OPTARG}" ;;
-	i ) IP_ADDRESS="${OPTARG}" ;;
-	r ) REBOOT_WAIT_SECS="${OPTARG}" ;;
-   \? ) echo "Invalid Option: -$OPTARG" 1>&2 && exit 1 ;;
-	: ) echo "Invalid Option: -$OPTARG requires an argument" 1>&2 && exit 1 ;;
+    n ) DRY_RUN=1 ;;
+    q ) QUIET=1 ;;
+    U ) USERNAME="${OPTARG}" ;;
+    i ) IP_ADDRESS="${OPTARG}" ;;
+    r ) REBOOT_WAIT_SECS="${OPTARG}" ;;
+    \? ) echo "Invalid Option: -$OPTARG" 1>&2 && exit 1 ;;
+    : ) echo "Invalid Option: -$OPTARG requires an argument" 1>&2 && exit 1 ;;
   esac
 done
 shift $((OPTIND -1))
@@ -20,13 +20,13 @@ shift $((OPTIND -1))
 #####################################################
 
 function say() {
-	if [[ "$QUIET" != 1 ]]; then
-		echo ""
-		echo "################################"
-		echo "##  $1"
-		echo "################################"
-		echo ""
-	fi
+  if [[ "$QUIET" != 1 ]]; then
+    echo ""
+    echo "################################"
+    echo "##  $1"
+    echo "################################"
+    echo ""
+  fi
 }
 
 # n. Set the hostname
@@ -38,12 +38,12 @@ HOSTS_FILE='/etc/hosts'
 REPLACED_HOSTS_CONTENT=$(sed s/$(hostname)/"$HOSTNAME"/g < "$HOSTS_FILE")
 
 if [[ "$DRY_RUN" == 1 ]]; then
-	echo "Would set ${HOSTNAME_FILE} content to ${HOSTNAME}"
-	echo "Would replace ${HOSTS_FILE} content with:"
-	echo "$REPLACED_HOSTS_CONTENT"
+  echo "Would set ${HOSTNAME_FILE} content to ${HOSTNAME}"
+  echo "Would replace ${HOSTS_FILE} content with:"
+  echo "$REPLACED_HOSTS_CONTENT"
 else
-	echo "$HOSTNAME" > "$HOSTNAME_FILE"
-	echo "$REPLACED_HOSTS_CONTENT" > "$HOSTS_FILE"
+  echo "$HOSTNAME" > "$HOSTNAME_FILE"
+  echo "$REPLACED_HOSTS_CONTENT" > "$HOSTS_FILE"
 fi
 
 # n. Set aliases
@@ -52,7 +52,7 @@ say "Setting aliases"
 if [[ "$DRY_RUN" == 1 ]]; then
     echo "Would write aliases to '/home/${USERNAME}/.bash_aliases'"
 else
-	cat > "/home/${USERNAME}/.bash_aliases" <<EOF
+  cat > "/home/${USERNAME}/.bash_aliases" <<EOF
 alias rp='source ~/.profile'
 alias ll='ls -lA --color'
 EOF
@@ -62,11 +62,11 @@ fi
 say "Setting variables"
 
 if [[ "$DRY_RUN" == 1 ]]; then
-	echo "Would write variables to '/home/${USERNAME}/.bash_variables'"
-	echo "Would add 'source ~/.bash_variables' to '/home/${USERNAME}/.bash_variables'"
+  echo "Would write variables to '/home/${USERNAME}/.bash_variables'"
+  echo "Would add 'source ~/.bash_variables' to '/home/${USERNAME}/.bash_variables'"
 else
-	echo "source ~/.bash_variables" >> "/home/${USERNAME}/.bashrc"
-    cat > "/home/${USERNAME}/.bash_variables" <<EOF
+  echo "source ~/.bash_variables" >> "/home/${USERNAME}/.bashrc"
+  cat > "/home/${USERNAME}/.bash_variables" <<EOF
 export PS1="\[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\n> "
 export PS2="\[\033[1;33m\]→ \[\033[m\]"
 EOF
@@ -79,37 +79,36 @@ DHCPCD_CONF_FILE='/etc/dhcpcd.conf'
 NETWORK_CONFIG="interface eth0\nstatic ip_address=${IP_ADDRESS}/24\nstatic routers=192.168.1.1\nstatic domain_name_servers=192.168.1.7"
 
 if [[ "$DRY_RUN" == 1 ]]; then
-	echo "Would add the following to ${DHCPCD_CONF_FILE}:"
-	echo -e "$NETWORK_CONFIG"
-	echo "Would run: systemctl disable dhcpcd"
-	echo "Would run: systemctl enable networking"
+  echo "Would add the following to ${DHCPCD_CONF_FILE}:"
+  echo -e "$NETWORK_CONFIG"
+  echo "Would run: systemctl disable dhcpcd"
+  echo "Would run: systemctl enable networking"
 else
-	echo -e "$NETWORK_CONFIG" >> "$DHCPCD_CONF_FILE"
-	systemctl disable dhcpcd
-	systemctl enable networking
+  echo -e "$NETWORK_CONFIG" >> "$DHCPCD_CONF_FILE"
+  systemctl disable dhcpcd
+  systemctl enable networking
 fi
 
 # n. Install packages
 say "Installing packages"
 
 if [[ "$DRY_RUN" == 1 ]]; then
-	echo "Would update apt and install packages."
+  echo "Would update apt and install packages."
 else
     apt-get update -y
     apt-get install -y \
-        curl \
-        wget \
-        vim \
-        git \
-        dnsutils
+      curl \
+      wget \
+      vim \
+      git \
+      dnsutils
 fi
 
 # n. Reboot
 say "Rebooting in ${REBOOT_WAIT_SECS} seconds"
 
 if [[ "$DRY_RUN" == 1 ]]; then
-	echo "Would run: sleep ${REBOOT_WAIT_SECS} && reboot"
+  echo "Would run: sleep ${REBOOT_WAIT_SECS} && reboot"
 else
-	sleep "$REBOOT_WAIT_SECS" && reboot
+  sleep "$REBOOT_WAIT_SECS" && reboot
 fi
-
